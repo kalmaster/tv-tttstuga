09c2adeb339bf91f082659da3d191aa9f57d3608
KilianJablonkaTe16 <kilian.jablonka16@itgksd.se>
2018-09-26 15:11:47 +0200

    Nya saker

--- Design.1/adminSideScript.js
+++ Design.1/adminSideScript.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
 
-alert("Hi");
-/*var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
+  /*  $("#calendar").datepicker({
+        inline: true,
+        firstDay: 1,
+        showOtherMonths: true,
+        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    }); */
+  
 
-alert("Hey");
-//Lägger ihop datumet av idag med månaden och året vi har just nu.
-var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-console.log(currentDate); */
+	var fullDate = new Date();
+	var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
 
- 
+	//Lägger ihop datumet av idag med månaden och året vi har just nu.
+	var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+	console.log(currentDate);
+
+ 	
     var today = new Date();
-    var weekno = today.getWeek();
 
-alert(weekno);
+    $("#r1k1").html(currentDate);
+    alert(currentDate);
 });
