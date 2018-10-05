fcb077b0bd44ebfc91793e169a410e3ef7329bfa
KilianJablonkaTe16 <kilian.jablonka16@itgksd.se>
2018-10-05 10:51:09 +0200

    Ändringar på veckoschemat

--- Design.1/insert_user.php
+++ Design.1/insert_user.php
@@ -8,6 +8,19 @@ if (!isset($_GET['register-user'])) {
 
 	$username = $_POST['username'];
 	$password = $_POST['password'];
+	$lagenhetsnr = $_POST['lagenhetsnr']; 
+	//$picture = $_FILE['picture'];
 
-	
+	$sql = "INSERT INTO users (apartmentnumber, fullname, password)
+	VALUES ($lagenhetsnr, $username, $password)";
+
+	if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+	} else {
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
+} else {
+	echo "in the else";
 }
--- Design.1/userSideScript.js
+++ Design.1/userSideScript.js
@@ -1,37 +1,5 @@
 $(document).ready(function() {
-
-	//$("#bokaTid").hide();
-
-  /*  $("#calendar").datepicker({
-        inline: true,
-        firstDay: 1,
-        showOtherMonths: true,
-        dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    }); */
-  
-/*
-	var fullDate = new Date();
-	var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
-
-	//Lägger ihop datumet av idag med månaden och året vi har just nu.
-	var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
-	console.log(currentDate);
-
- 	
-    var today = new Date();
-
-    $("#r1k1").html(currentDate);
-    alert(currentDate);
-
-        var date=new Date();
-    date.setFullYear(2020,1,1);
-    var _now=new Date();
-    if(date.getTime()>_now.getTime())
-        {
-      // date is future
-      		alert(date);
-        }
-*/	
+	
 	var d = new Date();
     var weekday = new Array(7);
 
@@ -58,47 +26,77 @@ $(document).ready(function() {
     		 }
     }
 
-		var date = new Date();
+	var date = new Date();
 
-		var day = date.getDate();
-		var month = date.getMonth();
-		var year = date.getFullYear();
-		var fullDate = new Date();  
+	var day = date.getDate();
+	var month = date.getMonth();
+	var year = date.getFullYear();
+	var fullDate = new Date();  
+	
+	$("#showYear").html(year);
+	var j = 0;
+
+	//Skriver ut alla datum från idag tills 30 dagar fram åt
+	for(var i = idStart; i < 30; i++)
+	{
+		//Lägger ihop datumet
+		fullDate.setFullYear(year, month, (day + j));
+		var formateradFullDate = fullDate.toLocaleDateString();
 		
-		$("#showYear").html(year);
-		var j = 0;
+		//Lägger in datumen i tabellen
+		$("#"+i).text(formateradFullDate);
+		j++;
+	} 
 
-		//Skriver ut alla datum från idag tills 30 dagar fram åt
-		for(var i = idStart; i < 30; i++)
+	//Lägger till datumet du har bokad i bokningsrutan
+	//när man väljer ett datum.
+	$(".datumTabel").click(function() {
+		//Tar fram id:t på det TD elementen som man trycktes på
+		var id = $(this).attr('id');
+
+		if($("#"+id).html() != "Ej bokbar")
 		{
-			//Lägger ihop datumet
-			fullDate.setFullYear(year, month, (day + j));
-			var formateradFullDate = fullDate.toLocaleDateString();
-			
-			//Lägger in datumen i tabellen
-			$("#"+i).text(formateradFullDate);
-			j++;
-		} 
-
-		//Lägger till datumet du har bokad i bokningsrutan
-		//när man väljer ett datum.
-		$(".datumTabel").click(function() {
-			//Tar fram id:t på det TD elementen som man trycktes på
-			var id = $(this).attr('id');
-
 			$(".datumTabel").css({"background-color": "gray", })
 			$("#"+id).css({"background-color": "red"})
 
+			for(var i = 0; i < 36; i++)
+			{
+				if($("#"+i).html() == "Ej bokbar")
+				{	
+					$("#"+i).css({"background-color": "red"})
+				}
+			}	
+
 			//Lägger till datumet du har valt i bokningsrutan
 			var datumString = $("#"+id).html();
-			$("#bokadDatum").html(datumString);
-		});
+			$("#bokadDatum").html(datumString);		
+		} 
 
-		//Lägger till tiden du har bokad i bokningsrutan 
-		//när man väljer en tid.
-		$("#tidSelect").change(function() {			
-			 $("#bokadTid").html($("#tidSelect option:checked").text());
-		});
+		else 
+		{
+			alert("Det datumet är ej bokbar.");
+		}
+
+
+
+
+	});
+
+	//Lägger till tiden du har bokad i bokningsrutan 
+	//när man väljer en tid.
+	$("#tidSelect").change(function() {			
+		 $("#bokadTid").html($("#tidSelect option:checked").text());
+	});
+
+
+	for(var i = 0; i < 36; i++)
+	{
+		if($("#"+i).html() == "")
+		{
+			$("#"+i).html("Ej bokbar");	
+			$("#"+i).css({"background-color": "red"})
+		}
+	}
 
 
 });
